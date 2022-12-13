008aa43d34f812165fe436bea870a462abd72698
Kacper Strugała <kacperstrugala5@gmail.com>
2022-12-13 20:24:41 +0100

    Day1_part2

--- Day1/main.py
+++ Day1/main.py
@@ -1,19 +1,21 @@
 with open("calories.txt", 'r')as f:
 
     read_file = f.read()
-    largest = 0
+
     elves = read_file.split("\n\n")
     total = 0
-
+    largest = [0,0,0]
     for elf in elves:
         x = elf.split()
         for x in elf.split():
             total += int(x)
 
-        if total > largest:
-            largest = total
+        #if total > largest:
+            #largest = total
+        largest = sorted(largest + [total], reverse=True)[:3]
 
         if x != int:
             total = 0
-print(f"The largest amount of calories is {largest}")
+#print(f"The largest amount of calories is {largest}")
 
+print(f"Sum of highest three elves: {sum(largest)}")
